082c2e4be1d10c763c8745d9180c9b80bf66d50c
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-06-25 16:54:57 +0900

    Building the Static App: Forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
 		<div className="app">
 			<div className="sidebar">
 				<FriendsList />
+				<FormAddFriend />
+				<Button>Add friend</Button>
 			</div>
+			<FormSplitBill />
 		</div>
 	);
 }
@@ -61,7 +64,50 @@ function Friend({ friend }) {
 
 			{friend.balance === 0 && <p>You and {friend.name} are even.</p>}
 
-			<button className="button">Select</button>
+			<Button>Select</Button>
 		</li>
+	);
+}
+
+function Button({ children }) {
+	return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+	return (
+		<form className="form-add-friend">
+			<label>👩🏻‍🤝‍👨🏼 Friend Name</label>
+			<input type="text" />
+
+			<label>🌄Image URL</label>
+			<input type="text" />
+
+			<Button>Add</Button>
+		</form>
+	);
+}
+
+function FormSplitBill() {
+	return (
+		<form className="form-split-bill">
+			<h2>Split a bill with X</h2>
+
+			<label>💰 Bill Value</label>
+			<input type="text" />
+
+			<label>🙍🏼 Your expense</label>
+			<input type="text" />
+
+			<label>👩🏻‍🤝‍👨🏼 X's expense</label>
+			<input type="text" disabled />
+
+			<label>🤑 Who is paying the bill</label>
+			<select>
+				<option value="user">You</option>
+				<option value="friend">X</option>
+			</select>
+
+			<Button>Split Bill</Button>
+		</form>
 	);
 }
